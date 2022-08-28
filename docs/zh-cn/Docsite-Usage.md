a57c2250f31fe4fbac61f64e89d948cc3b793dd1
LiuHui <whitetulips@163.com>
2022-08-28 13:10:00 +0800

    add usage doc

---
+++ docs/zh-cn/Docsite-Usage.md
@@ -0,0 +1,130 @@
+# Docsite Usage
+
+## 一、安装
+
+- [Docsite官网](https://docsite.js.org)
+- [Docsite Github](https://github.com/txd-team/docsite)
+- [Docsify官网](https://docsify.js.org)
+- [Valine官网](https://valine.js.org)
+
+```shell
+# node版本需要 > 6.x
+node -v
+npm -v
+
+# 全局安装Docsite
+npm install docsite -g
+```
+
+## 二、运行
+```shell
+# 初始化项目
+docsite init <project_name>
+
+# 示例
+cd /home/lorchr/ && docsite init light-docs
+mkdirdir ./light-docs && cd ./light-docs && docsite init
+
+# 预览
+docsite start
+
+# 打包
+docsite build
+```
+
+## 三、配置
+
+### 1. 修改网站标题等信息`docsite.config.yml`
+
+```yaml
+pages:
+ # key is the dirname of pages in src/pages
+ home:
+  # 首页配置
+  zh-cn:
+   title: 'Light Docs'
+   keywords: '关键词1，关键词2'
+   description: '页面内容简介'
+  # home config
+  en-us:
+   title: 'Light Docs'
+   keywords: 'keyword1,keyword2'
+   description: 'page description'
+ docs:
+  # 社区页配置
+  zh-cn:
+   title: 'Light Documentation'
+   keywords: '关键词1，关键词2'
+   description: '页面内容简介'
+  # community page config
+  en-us:
+   title: 'Light Documentation'
+   keywords: 'keyword1,keyword2'
+   description: 'page description'
+ blog:
+  # 博客列表页配置
+  zh-cn:
+   title: 'Light Blog'
+   keywords: '关键词1，关键词2'
+   description: '页面内容简介'
+  en-us:
+   # blog list page config
+   title: 'Light Blog'
+   keywords: 'keyword1,keyword2'
+   description: 'page description'
+ community:
+  # 社区页配置
+  zh-cn:
+   title: 'Light Community'
+   keywords: '关键词1，关键词2'
+   description: '页面内容简介'
+  # community page config
+  en-us:
+   title: 'Light Community'
+   keywords: 'keyword1,keyword2'
+   description: 'page description'
+```
+
+## 四、使用-文档
+1. 在`docs/zh-cn`目录下新建一个文件`Docsite-Usage.md`，并书写内容
+2. 将该文件复制到`docs/en-us`目录下
+3. 配置该文档到待生成的网页中`site_config/docs.js`
+```shell
+{
+  'en-us': {
+    sidemenu: [
+      {
+        title: 'Light Docs',
+        children: [
+          {
+            # 当前文档显示的大纲标题
+            title: 'Docsite Usage',
+            # 文档的路径
+            link: '/en-us/docs/Docsite-Usage.html',
+          }
+        ]
+      }
+    ]
+  },
+  'zh-cn': {
+    sidemenu: [
+      {
+        title: 'Light Docs',
+        children: [
+          {
+            # 当前文档显示的大纲标题
+            title: 'Docsite Usage',
+            # 文档的路径
+            link: '/zh-cn/docs/Docsite-Usage.html',
+          }
+        ]
+      }
+    ]
+  }
+}
+```
+4. 再次运行`docsite start`
+
+## 五、使用-博客
+
+## 六、使用-社区
--- site_config/docs.js
+++ site_config/docs.js
@@ -5,12 +5,38 @@ export default {
         title: 'Light Docs',
         children: [
           {
-            title: 'demo1',
-            link: '/en-us/docs/demo1.html',
+            title: 'Docsite Usage',
+            link: '/en-us/docs/Docsite-Usage.html',
           },
           {
-            title: 'demo2',
-            link: '/en-us/docs/demo2.html',
+            title: 'Spring Boot',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Boot',
+                link: '/en-us/docs/Spring-Boot/Spring-Boot.html',
+              },
+            ],
+          },
+          {
+            title: 'Spring Cloud',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Cloud',
+                link: '/en-us/docs/Spring-Cloud/Spring-Cloud.html',
+              },
+            ],
+          },
+          {
+            title: 'Spring Security',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Security',
+                link: '/en-us/docs/Spring-Security/Spring-Security.html',
+              },
+            ],
           },
           {
             title: 'CI/CD',
@@ -18,35 +44,35 @@ export default {
             children: [
               {
                 title: 'Install JDK',
-                link: '/en-us/docs/CI-CD/01-Install JDK.html',
+                link: '/en-us/docs/CI-CD/01-Install-JDK.html',
               },
               {
                 title: 'Install NodeJS',
-                link: '/en-us/docs/CI-CD/02-Install NodeJS.html',
+                link: '/en-us/docs/CI-CD/02-Install-NodeJS.html',
               },
               {
                 title: 'Install Maven',
-                link: '/en-us/docs/CI-CD/03-Install Maven.html',
+                link: '/en-us/docs/CI-CD/03-Install-Maven.html',
               },
               {
                 title: 'Install Gitea',
-                link: '/en-us/docs/CI-CD/04-Install Gitea.html',
+                link: '/en-us/docs/CI-CD/04-Install-Gitea.html',
               },
               {
                 title: 'Install Nexus3',
-                link: '/en-us/docs/CI-CD/05-Install Nexus3.html',
+                link: '/en-us/docs/CI-CD/05-Install-Nexus3.html',
               },
               {
                 title: 'Install Jenkins',
-                link: '/en-us/docs/CI-CD/06-Install Jenkins.html',
+                link: '/en-us/docs/CI-CD/06-Install-Jenkins.html',
               },
               {
                 title: 'Install Docker Registry',
-                link: '/en-us/docs/CI-CD/07-Install Docker Registry.html',
+                link: '/en-us/docs/CI-CD/07-Install-Docker-Registry.html',
               },
               {
                 title: 'Install Harbor',
-                link: '/en-us/docs/CI-CD/08-Install Harbor.html',
+                link: '/en-us/docs/CI-CD/08-Install-Harbor.html',
               },
             ],
           },
@@ -56,7 +82,7 @@ export default {
             children: [
               {
                 title: 'K8s Single Master Cluster',
-                link: '/en-us/docs/dir/K8s-Single-Master-Cluster.html',
+                link: '/en-us/docs/K8s/K8s-Single-Master-Cluster.html',
               },
             ],
           },
@@ -71,12 +97,38 @@ export default {
         title: 'Light Docs',
         children: [
           {
-            title: '示例1',
-            link: '/zh-cn/docs/demo1.html',
+            title: 'Docsite 使用',
+            link: '/zh-cn/docs/Docsite-Usage.html',
           },
           {
-            title: '示例2',
-            link: '/zh-cn/docs/demo2.html',
+            title: 'Spring Boot',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Boot',
+                link: '/zh-cn/docs/Spring-Boot/Spring-Boot.html',
+              },
+            ],
+          },
+          {
+            title: 'Spring Cloud',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Cloud',
+                link: '/zh-cn/docs/Spring-Cloud/Spring-Cloud.html',
+              },
+            ],
+          },
+          {
+            title: 'Spring Security',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Security',
+                link: '/zh-cn/docs/Spring-Security/Spring-Security.html',
+              },
+            ],
           },
           {
             title: 'CI/CD',
@@ -84,35 +136,35 @@ export default {
             children: [
               {
                 title: 'Install JDK',
-                link: '/zh-cn/docs/CI-CD/01-Install JDK.html',
+                link: '/zh-cn/docs/CI-CD/01-Install-JDK.html',
               },
               {
                 title: 'Install NodeJS',
-                link: '/zh-cn/docs/CI-CD/02-Install NodeJS.html',
+                link: '/zh-cn/docs/CI-CD/02-Install-NodeJS.html',
               },
               {
                 title: 'Install Maven',
-                link: '/zh-cn/docs/CI-CD/03-Install Maven.html',
+                link: '/zh-cn/docs/CI-CD/03-Install-Maven.html',
               },
               {
                 title: 'Install Gitea',
-                link: '/zh-cn/docs/CI-CD/04-Install Gitea.html',
+                link: '/zh-cn/docs/CI-CD/04-Install-Gitea.html',
               },
               {
                 title: 'Install Nexus3',
-                link: '/zh-cn/docs/CI-CD/05-Install Nexus3.html',
+                link: '/zh-cn/docs/CI-CD/05-Install-Nexus3.html',
               },
               {
                 title: 'Install Jenkins',
-                link: '/zh-cn/docs/CI-CD/06-Install Jenkins.html',
+                link: '/zh-cn/docs/CI-CD/06-Install-Jenkins.html',
               },
               {
                 title: 'Install Docker Registry',
-                link: '/zh-cn/docs/CI-CD/07-Install Docker Registry.html',
+                link: '/zh-cn/docs/CI-CD/07-Install-Docker Registry.html',
               },
               {
                 title: 'Install Harbor',
-                link: '/zh-cn/docs/CI-CD/08-Install Harbor.html',
+                link: '/zh-cn/docs/CI-CD/08-Install-Harbor.html',
               },
             ],
           },
@@ -122,7 +174,7 @@ export default {
             children: [
               {
                 title: 'K8s 单Master集群',
-                link: '/zh-cn/docs/dir/K8s-Single-Master-Cluster.html',
+                link: '/zh-cn/docs/K8s/K8s-Single-Master-Cluster.html',
               },
             ],
           },
