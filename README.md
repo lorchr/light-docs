869e9ea216e2671c6568cdea16152c6184ff9beb
lorchr <36924766+lorchr@users.noreply.github.com>
2022-09-05 03:13:37 +0000

    Deploying to site from @ lorchr/light-docs@ab1a4776758dd02702687fa6672c43d8eda0c560 🚀

---
+++ README.md
@@ -0,0 +1,28 @@
+# Dependency
+```shell
+node.js > 6.x
+```
+# Usage
+## Install
+```shell
+npm install docsite -g
+```
+
+## Init a project
+
+```shell
+$ cd my-project
+$ docsite init
+# or
+$ docsite init my-project
+```
+
+## Local preview
+```shell
+$ docsite start
+```
+
+## Build
+```shell
+$ docsite build
+```
---
+++ site_config/docs.js
@@ -0,0 +1,186 @@
+export default {
+  'en-us': {
+    sidemenu: [
+      {
+        title: 'Light Docs',
+        children: [
+          {
+            title: 'Docsite Usage',
+            link: '/en-us/docs/Docsite-Usage.html',
+          },
+          {
+            title: 'Spring Boot',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Boot',
+                link: '/en-us/docs/Spring-Boot/Spring-Boot.html',
+              },
+            ],
+          },
+          {
+            title: 'Spring Cloud',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Cloud',
+                link: '/en-us/docs/Spring-Cloud/Spring-Cloud.html',
+              },
+            ],
+          },
+          {
+            title: 'Spring Security',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Security',
+                link: '/en-us/docs/Spring-Security/Spring-Security.html',
+              },
+            ],
+          },
+          {
+            title: 'CI/CD',
+            opened: false,
+            children: [
+              {
+                title: 'Install JDK',
+                link: '/en-us/docs/CI-CD/01-Install-JDK.html',
+              },
+              {
+                title: 'Install NodeJS',
+                link: '/en-us/docs/CI-CD/02-Install-NodeJS.html',
+              },
+              {
+                title: 'Install Maven',
+                link: '/en-us/docs/CI-CD/03-Install-Maven.html',
+              },
+              {
+                title: 'Install Gitea',
+                link: '/en-us/docs/CI-CD/04-Install-Gitea.html',
+              },
+              {
+                title: 'Install Nexus3',
+                link: '/en-us/docs/CI-CD/05-Install-Nexus3.html',
+              },
+              {
+                title: 'Install Jenkins',
+                link: '/en-us/docs/CI-CD/06-Install-Jenkins.html',
+              },
+              {
+                title: 'Install Docker Registry',
+                link: '/en-us/docs/CI-CD/07-Install-Docker-Registry.html',
+              },
+              {
+                title: 'Install Harbor',
+                link: '/en-us/docs/CI-CD/08-Install-Harbor.html',
+              },
+            ],
+          },
+          {
+            title: 'K8s',
+            opened: false,
+            children: [
+              {
+                title: 'K8s Single Master Cluster',
+                link: '/en-us/docs/K8s/K8s-Single-Master-Cluster.html',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    barText: 'Documentation',
+  },
+  'zh-cn': {
+    sidemenu: [
+      {
+        title: 'Light Docs',
+        children: [
+          {
+            title: 'Docsite 使用',
+            link: '/zh-cn/docs/Docsite-Usage.html',
+          },
+          {
+            title: 'Spring Boot',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Boot',
+                link: '/zh-cn/docs/Spring-Boot/Spring-Boot.html',
+              },
+            ],
+          },
+          {
+            title: 'Spring Cloud',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Cloud',
+                link: '/zh-cn/docs/Spring-Cloud/Spring-Cloud.html',
+              },
+            ],
+          },
+          {
+            title: 'Spring Security',
+            opened: false,
+            children: [
+              {
+                title: 'Spring Security',
+                link: '/zh-cn/docs/Spring-Security/Spring-Security.html',
+              },
+            ],
+          },
+          {
+            title: 'CI/CD',
+            opened: false,
+            children: [
+              {
+                title: 'Install JDK',
+                link: '/zh-cn/docs/CI-CD/01-Install-JDK.html',
+              },
+              {
+                title: 'Install NodeJS',
+                link: '/zh-cn/docs/CI-CD/02-Install-NodeJS.html',
+              },
+              {
+                title: 'Install Maven',
+                link: '/zh-cn/docs/CI-CD/03-Install-Maven.html',
+              },
+              {
+                title: 'Install Gitea',
+                link: '/zh-cn/docs/CI-CD/04-Install-Gitea.html',
+              },
+              {
+                title: 'Install Nexus3',
+                link: '/zh-cn/docs/CI-CD/05-Install-Nexus3.html',
+              },
+              {
+                title: 'Install Jenkins',
+                link: '/zh-cn/docs/CI-CD/06-Install-Jenkins.html',
+              },
+              {
+                title: 'Install Docker Registry',
+                link: '/zh-cn/docs/CI-CD/07-Install-Docker Registry.html',
+              },
+              {
+                title: 'Install Harbor',
+                link: '/zh-cn/docs/CI-CD/08-Install-Harbor.html',
+              },
+            ],
+          },
+          {
+            title: 'K8s',
+            opened: false,
+            children: [
+              {
+                title: 'K8s 单Master集群',
+                link: '/zh-cn/docs/K8s/K8s-Single-Master-Cluster.html',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    barText: '文档',
+  },
+};
